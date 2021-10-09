ae63af27dbdacb4290e902e6509cc737ac14cd08
Amey <ameydeshingkar@gmail.com>
2021-10-09 20:47:40 +0530

    State management with provider package.

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -8,11 +10,13 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
-
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen:
+          false, //Here the listen is set to false as in product detail screen we do not want to update this screen whenever new data is added.
+    ).findById(productId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('title'),
-      ),
+      appBar: AppBar(title: Text(loadedProduct.title)),
     );
   }
 }
