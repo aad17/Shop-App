596d31af30e249b4b9fb58ed5e6297d8b47bdb86
Amey <ameydeshingkar@gmail.com>
2021-10-15 19:17:28 +0530

    Edit screen

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './edit_product_screen.dart';
+import '../widgets/app_drawer.dart';
+import '../widgets/user_product_item.dart';
+import '../providers/products.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-product';
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, EditProductScreen.routeName);
+            },
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductItem(
+                imageUrl: productsData.items[i].imageUrl,
+                title: productsData.items[i].title,
+              ),
+              Divider(),
+            ],
+          ),
+          itemCount: productsData.items.length,
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -51,8 +51,8 @@ class ProductItem extends StatelessWidget {
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
-              Scaffold.of(context).hideCurrentSnackBar();
-              Scaffold.of(context).showSnackBar(
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
                   content: Text(
                     'Added Item to Cart!!',
