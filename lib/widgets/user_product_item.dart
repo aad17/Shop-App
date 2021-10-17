44772c2c6e9400e27ee8b2cd9baec3988881a387
Amey <ameydeshingkar@gmail.com>
2021-10-17 18:50:55 +0530

    Delete product

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../screens/edit_product_screen.dart';
+import '../providers/products.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
   final String title;
   final String imageUrl;
 
-  UserProductItem({this.title, this.imageUrl, this.id});
+  UserProductItem({
+    this.title,
+    this.imageUrl,
+    this.id,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +34,9 @@ class UserProductItem extends StatelessWidget {
               color: Theme.of(context).primaryColor,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
             )
